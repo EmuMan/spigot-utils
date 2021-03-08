996238b3ecd349693222d1907c27785f66a55c28
BuildTools <unconfigured@null.spigotmc.org>
2021-03-08 15:27:34 -0800

    Changed .jar deployment and finished FullDynamicSidebar and PartialDynamicSidebar classes

---
+++ src/main/java/net/emuman/spigotutils/list/FullDynamicSidebar.java
@@ -0,0 +1,89 @@
+package net.emuman.spigottesting.utils;
+
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.*;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+public class FullDynamicSidebar extends PartialDynamicSidebar {
+
+    private final Map<Integer, Supplier<String>> lineUpdaters;
+    private final Map<Integer, Supplier<String>> bodyUpdaters;
+
+    public FullDynamicSidebar(String id, String title, Scoreboard board) {
+        super(id, title, board);
+        this.lineUpdaters = new HashMap<>();
+        this.bodyUpdaters = new HashMap<>();
+
+        for (int i = 0; i < 15; i++) {
+            String line = StringUtils.repeat(" ", i + 1);
+            addLine(line);
+        }
+    }
+
+    public FullDynamicSidebar(String id, String title) {
+        this(id, title, Bukkit.getScoreboardManager().getNewScoreboard());
+    }
+
+    public void setFullLine(int line, String text) {
+        assert line >= 0 && line < 15;
+        blankFullLine(line);
+        // TODO: Color codes will probably break this
+        int size = text.length();
+        // split the string up into different parts to distribute to the prefix, body, and suffix
+        setLinePrefix(line, text.substring(0, Math.min(size, 16)));
+        if (size > 16) setLineBody(line, text.substring(16, Math.min(size, 32)));
+        if (size > 32) setLineSuffix(line, text.substring(32, Math.min(size, 48)));
+    }
+
+    public void setFullLine(int line, Supplier<String> updater) {
+        assert line >= 0 && line < 15;
+        lineUpdaters.put(line, updater);
+    }
+
+    public void setLineBody(int line, String text) {
+        assert line >= 0 && line < 15;
+        if (text.length() > 16) text = text.substring(0, 16);
+        if (lines.contains(text)) return; // cannot be more than one of the same line visible
+        if (lines.get(line) != null && lines.get(line).equals(text)) return; // don't have to update the line, it is the same
+        if (lines.get(line) != null) {
+            // Push the current line off the screen before adding the new one
+            Score score = this.getObjective().getScore(lines.get(line));
+            score.setScore(-1);
+        }
+        // Add the entry to the team if it does not already exist so the prefix/suffix are applied
+        if (!teams.get(line).getEntries().contains(text)) teams.get(line).addEntry(text);
+        this.getObjective().getScore(text).setScore(14 - line);
+        lines.set(line, text);
+    }
+
+    public void setLineBody(int line, Supplier<String> updater) {
+        assert line >= 0 && line < 15;
+        bodyUpdaters.put(line, updater);
+    }
+
+    public void removeUpdaters(int line) {
+        super.removeUpdaters(line);
+        lineUpdaters.remove(line);
+        bodyUpdaters.remove(line);
+    }
+
+    public void blankFullLine(int line) {
+        super.blankFullLine(line);
+        blankLineBody(line);
+    }
+
+    public void blankLineBody(int line) {
+        setLineBody(line, StringUtils.repeat(" ", line + 1));
+    }
+
+    @Override
+    public void run() {
+        // update the corresponding lines for every updater
+        super.run();
+        lineUpdaters.forEach((line, updater) -> setFullLine(line, updater.get()));
+        bodyUpdaters.forEach((line, updater) -> setLineBody(line, updater.get()));
+    }
+}
---
+++ src/main/java/net/emuman/spigotutils/list/PartialDynamicSidebar.java
@@ -0,0 +1,121 @@
+package net.emuman.spigottesting.utils;
+
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.*;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+public class PartialDynamicSidebar extends BukkitRunnable {
+
+    private final Scoreboard board;
+    private final Objective objective;
+    private String title;
+
+    protected final List<String> lines;
+    protected final List<Team> teams;
+
+    private final Map<Integer, Supplier<String>> prefixUpdaters;
+    private final Map<Integer, Supplier<String>> suffixUpdaters;
+
+    public PartialDynamicSidebar(String id, String title, Scoreboard board) {
+        this.board = board;
+        this.objective = board.registerNewObjective(id, "dummy", title);
+        this.title = title;
+        this.teams = new ArrayList<>();
+        this.lines = new ArrayList<>();
+        this.prefixUpdaters = new HashMap<>();
+        this.suffixUpdaters = new HashMap<>();
+
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+    }
+
+    public PartialDynamicSidebar(String id, String title) {
+        this(id, title, Bukkit.getScoreboardManager().getNewScoreboard());
+    }
+
+    public void addLine(String body) {
+        if (teams.size() > 15) return;
+        if (body.length() > 16) body = body.substring(0, 16);
+        lines.add(body);
+        Team newTeam = board.registerNewTeam("sidebar_row_" + teams.size());
+        teams.add(newTeam);
+        newTeam.addEntry(body);
+        for (String line : lines) {
+            // Shift every line score up one so we can insert this line as the lowest score, 0
+            int currentScore = objective.getScore(line).getScore();
+            objective.getScore(line).setScore(currentScore + 1);
+        }
+        objective.getScore(body).setScore(0);
+    }
+
+    public void setLinePrefix(int line, String text) {
+        assert line >= 0 && line < teams.size();
+        if (text.length() > 16) text = text.substring(0, 16);
+        teams.get(line).setPrefix(text);
+    }
+
+    public void setLinePrefix(int line, Supplier<String> updater) {
+        assert line >= 0 && line < teams.size();
+        prefixUpdaters.put(line, updater);
+    }
+
+    public void setLineSuffix(int line, String text) {
+        assert line >= 0 && line < teams.size();
+        if (text.length() > 16) text = text.substring(0, 16);
+        teams.get(line).setSuffix(text);
+    }
+
+    public void setLineSuffix(int line, Supplier<String> updater) {
+        assert line >= 0 && line < teams.size();
+        suffixUpdaters.put(line, updater);
+    }
+
+    public void removeUpdaters(int line) {
+        prefixUpdaters.remove(line);
+        suffixUpdaters.remove(line);
+    }
+
+    public void blankFullLine(int line) {
+        blankLinePrefix(line);
+        blankLineSuffix(line);
+    }
+
+    public void blankLinePrefix(int line) {
+        setLinePrefix(line, "");
+    }
+
+    public void blankLineSuffix(int line) {
+        setLineSuffix(line, "");
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        this.objective.setDisplayName(title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Scoreboard getBoard() {
+        return board;
+    }
+
+    public Objective getObjective() {
+        return objective;
+    }
+
+    public int getLength() {
+        return lines.size();
+    }
+
+    @Override
+    public void run() {
+        // update the corresponding lines for every updater
+        prefixUpdaters.forEach((line, updater) -> setLinePrefix(line, updater.get()));
+        suffixUpdaters.forEach((line, updater) -> setLineSuffix(line, updater.get()));
+    }
+}
